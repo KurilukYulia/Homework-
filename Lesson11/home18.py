a28c15b8901095402b779455db271d1f72dbf943
KurilukYulia <kurilukulia@gmail.com>
2024-05-07 09:21:55 +0200

    creat new file

--- Lesson11/home18.py
+++ Lesson11/home18.py
@@ -1,13 +1,6 @@
 import json
 
-document = {
-    123456: ("Ania", 23),
-    789564: ("Bob", 18),
-    234780: ("Mia", 17),
-    785612: ("Ken", 28),
-    987634: ("Inna", 45),
-    567341: ("Karl", 34)
-}
+document = { 123456: ("Ania", 23), 789564: ("Bob", 18), 234780: ("Mia", 17), 785612: ("Ken", 28),987634: ("Inna", 45),567341: ("Karl", 34)}
 
 with open('document_file.json', 'w') as file:
     json.dump(document, file)
